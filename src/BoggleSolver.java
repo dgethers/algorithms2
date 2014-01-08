f42964a1897523f75f2d61e9ec686fe34d2529a4
DGethers <david.gethers@jcrew.com>
2014-01-08 15:58:31 -0500

    Fix for back tracking logic but some positions on the board are not being correctly updated as free after use.

--- src/BoggleSolver.java
+++ src/BoggleSolver.java
@@ -10,15 +10,24 @@ public class BoggleSolver {
     private static class Position {
         int x;
         int y;
+        int level;
+        char letter;
 
-        private Position(int x, int y) {
+        private Position(int x, int y, int level) {
             this.x = x;
             this.y = y;
+            this.level = level;
+        }
+
+        private Position(int x, int y, char letter) {
+            this.x = x;
+            this.y = y;
+            this.letter = letter;
         }
 
         @Override
         public String toString() {
-            return "(" + x + "," + y + ")";
+            return String.format("(%d,%d) %s@%d", x, y, letter, level);
         }
     }
 
@@ -41,59 +50,63 @@ public class BoggleSolver {
 
     // Returns the set of all valid words in the given Boggle board, as an Iterable.
     public Iterable<String> getAllValidWords(BoggleBoard board) {
-        boolean[][] marked = new boolean[board.rows()][board.cols()];
-
 
         for (int row = 0; row < board.rows(); row++) {
-            columnLoop:
             for (int column = 0; column < board.cols(); column++) {
-                Stack<Position> history = new Stack<Position>();
+                boolean[][] marked = new boolean[board.rows()][board.cols()];
                 StringBuilder word = new StringBuilder();
-
                 char letter = board.getLetter(row, column);
                 word.append(letter);
-                Position current = new Position(row, column);
+
+                Stack<Position> history = new Stack<Position>();
+                Position pos = new Position(row, column, letter);
                 marked[row][column] = true;
-                List<Position> nextPositions = getNextPositions(current, board.rows(), board.cols());
+                List<Position> nextPositions = getNextPositions(board, pos, board.rows(), board.cols());
                 for (Position position : nextPositions) {
                     history.push(position);
                 }
 
+                int currentLevel = 0;
+
                 while (history.size() > 0) {
-                    Stack<Position> child = new Stack<Position>();
-                    child.push(history.pop());
+                    Position next = history.pop();
 
-                    while (child.size() > 0) {
-                        Position next = child.pop();
-                        marked[next.x][next.y] = true;
-                        word.append(board.getLetter(next.x, next.y));
-
-                        //not a prefix and not a word
-                        if (!(trie.keysWithPrefix(word.toString()).iterator().hasNext()) && trie.get(word.toString()) == null) {
-                            clearMarkedArray(board, marked);
-                            word.deleteCharAt(word.length() - 1);
-                            marked[next.x][next.y] = false;
-                        } //not a prefix but is a word
-                        else if (!trie.keysWithPrefix(word.toString()).iterator().hasNext() && trie.get(word.toString()) != null) {
-                            addWordToValidWordList(word.toString());
-                            clearMarkedArray(board, marked);
-                            continue columnLoop;
-                        } //is a prefix, is a word
-                        else if (trie.keysWithPrefix(word.toString()).iterator().hasNext() && trie.get(word.toString()) != null) {
-                            addWordToValidWordList(word.toString());
-                            addNextPositionsToStack(board, marked, child, next);
-                        } //is a prefix but not a word
-                        else {
-                            addNextPositionsToStack(board, marked, child, next);
-                        }
+                    boolean shouldRemoveLetter = next.level < currentLevel;
+                    if (shouldRemoveLetter) {
+                        currentLevel--;
+                        word.deleteCharAt(word.length() - 1);
                     }
 
-                    if (word.length() > 1)
-                        word.deleteCharAt(word.length() - 1);
+                    marked[next.x][next.y] = true;
+                    word.append(board.getLetter(next.x, next.y));
+
+                    //current entries do not form a word or are not a prefix for another. remove letter and keep trying
+                    if (!(trie.keysWithPrefix(word.toString()).iterator().hasNext()) && trie.get(word.toString()) == null) {
+//                        clearMarkedArray(board, marked);
+                        word.deleteCharAt(word.length() - 1); //delete last character since it isn't valid
+                        marked[next.x][next.y] = false; //mark it's entry to false since it is no longer used for word
+//                        currentLevel--;
+                    } //current entries form a word but do not form a prefix for any other words. add word to list and move on
+                    else if (!trie.keysWithPrefix(word.toString()).iterator().hasNext() && trie.get(word.toString()) != null) {
+                        addWordToValidWordList(word.toString());
+//                        clearMarkedArray(board, marked);
+                        marked[next.x][next.y] = false;
+                        word.deleteCharAt(word.length() - 1); //delete last character since it isn't valid
+//                        continue columnLoop;
+                    } //is a prefix, is a word
+                    else if (trie.keysWithPrefix(word.toString()).iterator().hasNext() && trie.get(word.toString()) != null) {
+                        addWordToValidWordList(word.toString());
+                        addNextPositionsToStack(board, marked, history, next);
+                        currentLevel++;
+                    } //is a prefix but not a word
+                    else if (trie.keysWithPrefix(word.toString()).iterator().hasNext() && trie.get(word.toString()) == null) {
+                        addNextPositionsToStack(board, marked, history, next);
+                        currentLevel++;
+                    }
                 }
             }
 
-            clearMarkedArray(board, marked);
+//            clearMarkedArray(board, marked);
         }
 
         return validWords;
@@ -105,10 +118,13 @@ public class BoggleSolver {
     }
 
     private void addNextPositionsToStack(BoggleBoard board, boolean[][] marked, Stack<Position> stack, Position next) {
-        List<Position> positions = getNextPositions(next, board.rows(), board.cols());
+        List<Position> positions = getNextPositions(board, next, board.rows(), board.cols());
         for (Position position : positions) {
             if (!marked[position.x][position.y]) {
-                stack.push(position);
+                position.level = position.level++;
+                Position newPosition = new Position(position.x, position.y, position.level + 1);
+                newPosition.letter = board.getLetter(position.x, position.y);
+                stack.push(newPosition);
             }
         }
     }
@@ -121,32 +137,32 @@ public class BoggleSolver {
         }
     }
 
-    private List<Position> getNextPositions(Position coordinates, int maxRows, int maxColumns) {
+    private List<Position> getNextPositions(BoggleBoard board, Position coordinates, int maxRows, int maxColumns) {
         List<Position> positions = new ArrayList<Position>();
 
         if (coordinates.x > 0)
-            positions.add(new Position(coordinates.x - 1, coordinates.y));
+            positions.add(new Position(coordinates.x - 1, coordinates.y, board.getLetter(coordinates.x - 1, coordinates.y)));
 
         if (coordinates.x > 0 && coordinates.y < maxColumns - 1)
-            positions.add(new Position(coordinates.x - 1, coordinates.y + 1));
+            positions.add(new Position(coordinates.x - 1, coordinates.y + 1, board.getLetter(coordinates.x - 1, coordinates.y + 1)));
 
         if (coordinates.y < maxColumns - 1)
-            positions.add(new Position(coordinates.x, coordinates.y + 1));
+            positions.add(new Position(coordinates.x, coordinates.y + 1, board.getLetter(coordinates.x, coordinates.y + 1)));
 
         if (coordinates.y < maxColumns - 1 && coordinates.x < maxRows - 1)
-            positions.add(new Position(coordinates.x + 1, coordinates.y + 1));
+            positions.add(new Position(coordinates.x + 1, coordinates.y + 1, board.getLetter(coordinates.x + 1, coordinates.y + 1)));
 
         if (coordinates.x < maxRows - 1)
-            positions.add(new Position(coordinates.x + 1, coordinates.y));
+            positions.add(new Position(coordinates.x + 1, coordinates.y, board.getLetter(coordinates.x + 1, coordinates.y)));
 
         if (coordinates.y > 0 && coordinates.x > 0)
-            positions.add(new Position(coordinates.x - 1, coordinates.y - 1));
+            positions.add(new Position(coordinates.x - 1, coordinates.y - 1, board.getLetter(coordinates.x - 1, coordinates.y - 1)));
 
         if (coordinates.y > 0)
-            positions.add(new Position(coordinates.x, coordinates.y - 1));
+            positions.add(new Position(coordinates.x, coordinates.y - 1, board.getLetter(coordinates.x, coordinates.y - 1)));
 
         if (coordinates.x < maxRows - 1 && coordinates.y > 0)
-            positions.add(new Position(coordinates.x + 1, coordinates.y - 1));
+            positions.add(new Position(coordinates.x + 1, coordinates.y - 1, board.getLetter(coordinates.x + 1, coordinates.y - 1)));
 
         return positions;
     }
